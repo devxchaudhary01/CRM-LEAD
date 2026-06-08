6277d9e3c4d40a266cda6b743760945f169f515b
Sachin Choudhary <devxchaudhary01@gmail.com>
2026-06-08 12:19:12 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:5173',
+    'https://crm-lead-wine.vercel.app',
     process.env.FRONTEND_URL,
   ].filter(Boolean),
   credentials: true,
